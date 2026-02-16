cbf67994ee63c8e2d96ca1aa9a4b4026edf8509b
suarniharsh <harshsurani00@gmail.com>
2026-02-16 16:33:19 +0000

    telescope added 🚀

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,4 +34,24 @@ require("lazy").setup({
       vim.cmd.colorscheme("catppuccin")
     end,
   },
-})
+ -- ======================
+  -- Telescope
+  -- ======================
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = "Telescope", -- lazy-load on command
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          prompt_prefix = "🔍 ",
+          selection_caret = "➜ ",
+          path_display = { "smart" },
+        },
+      })
+    end,
+  }
+  })
